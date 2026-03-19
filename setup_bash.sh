1eb0c12a213d27e8ca69d49b3d1728a149bdc313
d-laub <dlaub@ucsd.edu>
2026-03-18 17:04:43 -0700

    update setup

--- setup_bash.sh
+++ setup_bash.sh
@@ -3,20 +3,21 @@
 set -euo pipefail
 
 # install pixi and oh-my-bash
-curl -fsSL https://pixi.sh/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | sh
-
-# install global tools
-pixi g i ripgrep bat glow-md sd zoxide rnr fd-find exa prek git gh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # download and add aliases
 curl -fsSL https://raw.githubusercontent.com/d-laub/dlaub-togo/main/aliases.sh >> ~/.bash_aliases
 
 # download custom theme for oh-my-bash
-git clone --depth 1 --branch master https://github.com/d-laub/dlaub-togo.git /tmp/dlaub-togo-tmp
+git clone --depth 1 --branch main https://github.com/d-laub/dlaub-togo.git /tmp/dlaub-togo-tmp
 mkdir -p ~/.oh-my-bash/themes
 cp -r /tmp/dlaub-togo-tmp/agnoster-multiline ~/.oh-my-bash/themes/
 rm -rf /tmp/dlaub-togo-tmp
+curl -fsSL https://pixi.sh/install.sh | sh
+
+# install global tools
+pixi g i ripgrep bat glow-md sd zoxide rnr fd-find exa prek git gh
+echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 
 # set custom theme for oh-my-bash
 sd '^OSH_THEME=.*$' 'OSH_THEME="agnoster-multiline"' ~/.bashrc
