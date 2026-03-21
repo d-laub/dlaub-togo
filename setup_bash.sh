4da135b9eb2c2b9d002df69f2fca4a202a6c8804
David Laub <60826163+d-laub@users.noreply.github.com>
2026-03-21 15:43:46 -0700

    Update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -2,7 +2,13 @@
 
 set -euo pipefail
 
-# install pixi and oh-my-bash
+# install global tools
+curl -fsSL https://pixi.sh/install.sh | sh
+export PATH="${HOME}/.pixi/bin:${PATH}"
+pixi g i ripgrep bat glow-md sd zoxide rnr fd-find exa prek git gh less zellij
+echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
+
+# install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # download and add aliases
@@ -13,15 +19,8 @@ mkdir -p ~/.oh-my-bash/themes
 cp -r agnoster-multiline ~/.oh-my-bash/themes/
 sd '^OSH_THEME=.*$' 'OSH_THEME="agnoster-multiline"' ~/.bashrc
 
-# install global tools
-curl -fsSL https://pixi.sh/install.sh | sh
-export PATH="${HOME}/.pixi/bin:${PATH}"
-pixi g i ripgrep bat glow-md sd zoxide rnr fd-find exa prek git gh less zellij
-echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
-
 # config zellij
 mkdir -p ~/.config/zellij
 cp zellij_config.kdl ~/.config/zellij/config.kdl
 
 echo 'Finished setting up shell environment from dlaub-togo, reload with "source ~/.bashrc" for changes to take effect.'
-
