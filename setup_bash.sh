5d294553e6533e9aa55795f4da9018f666e60eab
David Laub <60826163+d-laub@users.noreply.github.com>
2026-04-25 13:03:53 -0700

    Update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" "" --unattended
 
 # all ~/.bashrc edits must be after oh-my-bash install
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:${PATH}"
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 
 # install global tools
