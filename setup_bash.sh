f732e4e4a23d0d4282ad1270499036d7ef592446
David Laub <60826163+d-laub@users.noreply.github.com>
2026-04-25 13:26:13 -0700

    Update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -23,6 +23,7 @@ curl -fsSL https://claude.ai/install.sh | bash
 curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | sh
 rtk init --global
 claude plugin marketplace add JuliusBrussee/caveman && claude plugin install caveman@caveman
+claude plugin marketplace add anthropics/claude-plugins-official
 claude plugin install superpowers@claude-plugins-official
 claude plugin install code-review@claude-plugins-official
 claude plugin install feature-dev@claude-plugins-official
