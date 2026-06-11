3e8b689cd81813794118ef1809634bcbee707a99
David Laub <60826163+d-laub@users.noreply.github.com>
2026-06-11 14:50:49 -0700

    Update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -59,6 +59,7 @@ npx -y skills add runpod/skills --agent claude-code --global -y
 npx -y skills add ML4GLand/SeqPro --skill seqpro --agent claude-code --global -y
 npx -y skills add d-laub/genoray --agent claude-code --global -y
 npx -y skills add mcvickerlab/GenVarLoader --agent claude-code --global -y
+npx -y skills add d-laub/xolars --agent claude-code --global -y
 
 # download and add aliases
 cat aliases.sh >> "${HOME}/.bash_aliases"
